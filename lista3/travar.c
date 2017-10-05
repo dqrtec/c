b9f628a328028a9d46f97b9b872959e02656d8e7
danieç <edaybrasil@gmail.com>
2017-10-05 12:46:02 -0300

    final
    +

--- lista3/travar.c
+++ lista3/travar.c
@@ -5,31 +5,17 @@ int main(int argc, char const *argv[])
 {
 	int soma=1000000000;
 	printf("%d\n",soma);
-	int *v = (int *) malloc(soma*sizeof (int));
-	int *a = (int *) malloc(1000000000*sizeof (int));
-	int *b = (int *) malloc(1000000000*sizeof (int));
-	int *c = (int *) malloc(1000000000*sizeof (int));
-	int *d = (int *) malloc(1000000000*sizeof (int));
-	int *e = (int *) malloc(1000000000*sizeof (int));
-	int *f = (int *) malloc(1000000000*sizeof (int));
-	printf("%d\n",soma);
+	int **v = (int **) malloc(soma*sizeof (int*));
 
-	for (int i = 1; i < 500000000000000000; ++i)
+int i;
+int j;
+	for (i = 1; i < soma; ++i)
 	{
-		for (int j = 1; j < 500000000000000000; ++j)
-		{
-			//printf("%d\n",j);
-			soma=soma*1.1;
-			printf("%d\n",soma);
-			v = (int *) realloc(v, ((soma)*sizeof(int)) );
-			a = (int *) realloc(a, ((soma)*sizeof(int)) );
-			b = (int *) realloc(b, ((soma)*sizeof(int)) );
-			c = (int *) realloc(c, ((soma)*sizeof(int)) );
-			d = (int *) realloc(d, ((soma)*sizeof(int)) );
-			e = (int *) realloc(e, ((soma)*sizeof(int)) );
-			f = (int *) realloc(f, ((soma)*sizeof(int)) );
+		v[i] = (int *) malloc(soma*sizeof (int));
+		
+		for(j=0; j<soma; j++){
+			v[i][j] = 0;
 		}
 	}
-	//free(v);
 	return 0;
 }
